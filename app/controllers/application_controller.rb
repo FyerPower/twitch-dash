73ef47201e8f77ac4aa31b95c7329422c7972a56
Michael Lynch <mlynch@cardinalsolutions.com>
2016-07-20 11:11:40 -0400

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  before_filter :redirect_to_https
   after_filter :set_csrf_cookie_for_ng
 
   # respond_to :json
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  def redirect_to_https
+    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
   end
 
   # def verified_request?
